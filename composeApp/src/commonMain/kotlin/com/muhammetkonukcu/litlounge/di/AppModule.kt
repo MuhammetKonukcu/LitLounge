c536515ef11f8bfcc63fd8d5e25fc0575373e90f
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-06-22 14:28:59 +0300

    feat(di): register ViewModels in appModule and invoke in initKoin.

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/di/AppModule.kt
@@ -1,11 +1,24 @@
 package com.muhammetkonukcu.litlounge.di
 
+import com.muhammetkonukcu.litlounge.viewmodel.HistoryViewModel
+import com.muhammetkonukcu.litlounge.viewmodel.HomeViewModel
+import com.muhammetkonukcu.litlounge.viewmodel.ProfileViewModel
 import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.module
+
+private fun appModule(): Module = module {
+    single<HomeViewModel> { HomeViewModel() }
+    single<HistoryViewModel> { HistoryViewModel() }
+    single<ProfileViewModel> { ProfileViewModel() }
+}
 
 fun initKoin(config: KoinAppDeclaration? = null) =
     startKoin {
         config?.invoke(this)
 
-        modules()
+        modules(
+            appModule()
+        )
     }
