fee1c4cd9d4ba468aff6f34312007cf93de15d07
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-06-28 13:23:40 +0300

    feat(viewmodel): inject BooksRepository into AddBookViewModel.

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/di/AppModule.kt
@@ -1,9 +1,13 @@
 package com.muhammetkonukcu.litlounge.di
 
+import com.muhammetkonukcu.litlounge.room.dao.BooksDao
 import com.muhammetkonukcu.litlounge.room.dao.UsersDao
 import com.muhammetkonukcu.litlounge.room.database.AppDatabase
+import com.muhammetkonukcu.litlounge.room.repository.BooksRepository
+import com.muhammetkonukcu.litlounge.room.repository.BooksRepositoryImpl
 import com.muhammetkonukcu.litlounge.room.repository.UsersRepository
 import com.muhammetkonukcu.litlounge.room.repository.UsersRepositoryImpl
+import com.muhammetkonukcu.litlounge.viewmodel.AddBookViewModel
 import com.muhammetkonukcu.litlounge.viewmodel.HistoryViewModel
 import com.muhammetkonukcu.litlounge.viewmodel.HomeViewModel
 import com.muhammetkonukcu.litlounge.viewmodel.ProfileViewModel
@@ -16,11 +20,15 @@ private fun appModule(): Module = module {
     single<HomeViewModel> { HomeViewModel(get()) }
     single<HistoryViewModel> { HistoryViewModel() }
     single<ProfileViewModel> { ProfileViewModel(get()) }
+    single<AddBookViewModel> { AddBookViewModel(get()) }
 }
 
 private fun repoModule(): Module = module {
     single<UsersDao> { get<AppDatabase>().getUsersDao() }
     single<UsersRepository> { UsersRepositoryImpl(get()) }
+
+    single<BooksDao> { get<AppDatabase>().getBooksDao() }
+    single<BooksRepository> { BooksRepositoryImpl(get()) }
 }
 
 expect fun databaseModule(): Module
