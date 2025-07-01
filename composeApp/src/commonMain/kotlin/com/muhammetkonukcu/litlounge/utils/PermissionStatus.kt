4bc32341be6489577dd9aac20241b70ca55ee1e8
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-07-01 17:45:46 +0300

    feat: add PermissionStatus.kt and PermissionType.kt

---
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/utils/PermissionStatus.kt
@@ -0,0 +1,7 @@
+package com.muhammetkonukcu.litlounge.utils
+
+enum class PermissionStatus {
+    GRANTED,
+    DENIED,
+    SHOW_RATIONAL
+}
---
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/utils/PermissionType.kt
@@ -0,0 +1,6 @@
+package com.muhammetkonukcu.litlounge.utils
+
+enum class PermissionType {
+    CAMERA,
+    GALLERY
+}
