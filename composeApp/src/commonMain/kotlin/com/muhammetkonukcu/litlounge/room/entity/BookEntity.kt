4805103b726e0adea40717d36c0a6d12082a037d
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-07-03 15:54:50 +0300

    feat: add authorName field .

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/entity/BookEntity.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/entity/BookEntity.kt
@@ -11,6 +11,7 @@ data class BookEntity(
     val totalPage: Int = 0,
     val currentPage: Int = 0,
     val imageURL: String = "",
+    val authorName: String = "",
     val startTimestamp: Long = 0,
     val finishTimestamp: Long = 0,
     val finished: Boolean = false,
