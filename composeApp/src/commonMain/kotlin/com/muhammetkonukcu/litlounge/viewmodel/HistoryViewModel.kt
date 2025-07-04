6dd05096e30efe415e6cee6c3b51509ffcf81b89
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-07-04 09:48:48 +0300

    feat(viewmodel): inject BooksRepository into HistoryViewModel and add historyPagingDataFlow using Paging.

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/viewmodel/HistoryViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/viewmodel/HistoryViewModel.kt
@@ -1,6 +1,15 @@
 package com.muhammetkonukcu.litlounge.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.cash.paging.PagingData
+import app.cash.paging.cachedIn
+import com.muhammetkonukcu.litlounge.room.entity.BookEntity
+import com.muhammetkonukcu.litlounge.room.repository.BooksRepository
+import kotlinx.coroutines.flow.Flow
 
-class HistoryViewModel: ViewModel() {
+class HistoryViewModel(private val booksRepository: BooksRepository) : ViewModel() {
+    val historyPagingDataFlow: Flow<PagingData<BookEntity>> =
+        booksRepository.getBooks()
+            .cachedIn(viewModelScope)
 }
