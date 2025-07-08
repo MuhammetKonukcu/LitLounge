581db7acafb9bf5fc034ac9e7e4ba17550f5e1d3
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-07-08 15:04:56 +0300

    feat(db): add PageEntity and register PageTrackDao in AppDatabase.

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/database/AppDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/database/AppDatabase.kt
@@ -3,12 +3,15 @@ package com.muhammetkonukcu.litlounge.room.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.muhammetkonukcu.litlounge.room.dao.BooksDao
+import com.muhammetkonukcu.litlounge.room.dao.PageTrackDao
 import com.muhammetkonukcu.litlounge.room.dao.UsersDao
 import com.muhammetkonukcu.litlounge.room.entity.BookEntity
+import com.muhammetkonukcu.litlounge.room.entity.PageEntity
 import com.muhammetkonukcu.litlounge.room.entity.UserEntity
 
-@Database(entities = [UserEntity::class, BookEntity::class], version = 1)
+@Database(entities = [UserEntity::class, BookEntity::class, PageEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getUsersDao(): UsersDao
     abstract fun getBooksDao(): BooksDao
+    abstract fun getPageTrackDao(): PageTrackDao
 }
