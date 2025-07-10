e79bb7a12cf32935a1dc5224b825996c92525b8e
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-07-10 20:50:47 +0300

    feat(db): add getFullPageTrack(pageSize) method with Paging support in PageTrackRepository and its implementation.

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/repository/PageTrackRepository.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/repository/PageTrackRepository.kt
@@ -1,8 +1,11 @@
 package com.muhammetkonukcu.litlounge.room.repository
 
+import androidx.paging.PagingData
 import com.muhammetkonukcu.litlounge.room.entity.PageEntity
+import kotlinx.coroutines.flow.Flow
 
 interface PageTrackRepository {
     suspend fun insertOrUpdatePage(entity: PageEntity)
     suspend fun getPageByDate(dateStr: String): PageEntity?
+    fun getFullPageTrack(pageSize: Int = 10): Flow<PagingData<PageEntity>>
 }
--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/repository/PageTrackRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/room/repository/PageTrackRepositoryImpl.kt
@@ -1,7 +1,11 @@
 package com.muhammetkonukcu.litlounge.room.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import app.cash.paging.PagingData
 import com.muhammetkonukcu.litlounge.room.dao.PageTrackDao
 import com.muhammetkonukcu.litlounge.room.entity.PageEntity
+import kotlinx.coroutines.flow.Flow
 
 class PageTrackRepositoryImpl(private val pageTrackDao: PageTrackDao) : PageTrackRepository {
     override suspend fun insertOrUpdatePage(entity: PageEntity) =
@@ -9,4 +13,14 @@ class PageTrackRepositoryImpl(private val pageTrackDao: PageTrackDao) : PageTrac
 
     override suspend fun getPageByDate(dateStr: String): PageEntity? =
         pageTrackDao.getPageByDate(dateStr)
+
+    override fun getFullPageTrack(pageSize: Int): Flow<PagingData<PageEntity>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = pageSize,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { pageTrackDao.getFullPageTrack() }
+        ).flow
+    }
 }
