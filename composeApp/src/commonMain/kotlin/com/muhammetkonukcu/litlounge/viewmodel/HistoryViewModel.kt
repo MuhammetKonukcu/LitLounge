29c17d16aa5f5af22a030176a06e5317a3275a3a
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-07-22 10:50:09 +0300

    feat(viewmodel): add removeBookFromDatabase to HistoryViewModel for deleting books.

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/viewmodel/HistoryViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/viewmodel/HistoryViewModel.kt
@@ -7,9 +7,16 @@ import app.cash.paging.cachedIn
 import com.muhammetkonukcu.litlounge.room.entity.BookEntity
 import com.muhammetkonukcu.litlounge.room.repository.BooksRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 class HistoryViewModel(private val booksRepository: BooksRepository) : ViewModel() {
     val historyPagingDataFlow: Flow<PagingData<BookEntity>> =
         booksRepository.getBooks()
             .cachedIn(viewModelScope)
+
+    fun removeBookFromDatabase(bookId: Int) {
+        viewModelScope.launch {
+            booksRepository.deleteBook(bookId)
+        }
+    }
 }
