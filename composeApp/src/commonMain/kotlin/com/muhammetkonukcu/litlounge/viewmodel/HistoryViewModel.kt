0abec9a17b60e9acefa376ca22aea610a71c0a16
Muhammet Konukcu <muhammedknkc@gmail.com>
2025-07-28 13:36:31 +0300

    feat(viewmodel): add dynamic filtering in HistoryViewModel with StateFlow and flatMapLatest
    - Introduce `_currentFilter` MutableStateFlow for ReadingFilter
    - Use `flatMapLatest` to switch between `getBooks()`, `getCurrentlyReadBooks()`, and `getFinishedBooks()`
    - Add `getBooksByFilter` to update filter value

--- composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/viewmodel/HistoryViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/muhammetkonukcu/litlounge/viewmodel/HistoryViewModel.kt
@@ -6,13 +6,30 @@ import app.cash.paging.PagingData
 import app.cash.paging.cachedIn
 import com.muhammetkonukcu.litlounge.room.entity.BookEntity
 import com.muhammetkonukcu.litlounge.room.repository.BooksRepository
+import com.muhammetkonukcu.litlounge.utils.ReadingFilter
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
 
 class HistoryViewModel(private val booksRepository: BooksRepository) : ViewModel() {
-    val historyPagingDataFlow: Flow<PagingData<BookEntity>> =
-        booksRepository.getBooks()
-            .cachedIn(viewModelScope)
+    private val _currentFilter = MutableStateFlow(ReadingFilter.ALL)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val historyPagingDataFlow: Flow<PagingData<BookEntity>> = _currentFilter
+        .flatMapLatest { filter ->
+            when (filter) {
+                ReadingFilter.ALL -> booksRepository.getBooks()
+                ReadingFilter.READING -> booksRepository.getCurrentlyReadBooks()
+                ReadingFilter.FINISHED -> booksRepository.getFinishedBooks()
+            }
+        }
+        .cachedIn(viewModelScope)
+
+    fun getBooksByFilter(filter: ReadingFilter) {
+        _currentFilter.value = filter
+    }
 
     fun removeBookFromDatabase(bookId: Int) {
         viewModelScope.launch {
